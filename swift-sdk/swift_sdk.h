a277a28e43dc7bb70643dd01d0dae38f00d2c8a3
Tapash Majumder <tapash@iterable.com>
2018-06-11 17:29:01 -0700

    Initial Version.

---
+++ swift-sdk/swift_sdk.h
@@ -0,0 +1,19 @@
+//
+//  swift_sdk.h
+//  swift-sdk
+//
+//  Created by Tapash Majumder on 6/11/18.
+//  Copyright © 2018 Iterable. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for swift_sdk.
+FOUNDATION_EXPORT double swift_sdkVersionNumber;
+
+//! Project version string for swift_sdk.
+FOUNDATION_EXPORT const unsigned char swift_sdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <swift_sdk/PublicHeader.h>
+
+
