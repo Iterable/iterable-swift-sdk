6a84989bdaa859054cc44b51229e2625aa80b4cd
Tapash Majumder <tapash@iterable.com>
2018-06-11 18:06:18 -0700

    Change Module Name to IterableSDK

--- swift-sdk/IterableSDK.h
+++ swift-sdk/IterableSDK.h
@@ -16,6 +16,6 @@ FOUNDATION_EXPORT const unsigned char swift_sdkVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <swift_sdk/PublicHeader.h>
 
-#import <swift_sdk/IterableConstants.h>
-#import <swift_sdk/IterableDateUtil.h>
-#import <swift_sdk/NSData+Conversion.h>
+#import <IterableSDK/IterableConstants.h>
+#import <IterableSDK/IterableDateUtil.h>
+#import <IterableSDK/NSData+Conversion.h>
