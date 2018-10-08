ab8090dc2d91a26437bce9187ed4170a45e8cfce
Tapash Majumder <tapash@iterable.com>
2018-10-08 10:24:37 -0700

    Try running one app at a time.

--- Tests/ui-tests/UITests.swift
+++ Tests/ui-tests/UITests.swift
@@ -8,22 +8,27 @@ import XCTest
 
 
 class UITests: XCTestCase {
-    static var application: XCUIApplication = {
-        let app = XCUIApplication()
-        app.launch()
-        return app
-    }()
-
-    // shortcut calculated property
-    private var app: XCUIApplication {
-        return UITests.application
-    }
+//!!!
+//    static var application: XCUIApplication = {
+//        let app = XCUIApplication()
+//        app.launch()
+//        return app
+//    }()
+//
+//    // shortcut calculated property
+//    private var app: XCUIApplication {
+//        return UITests.application
+//    }
+    private var app: XCUIApplication!
     
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
 
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
+
+        app = XCUIApplication()
+        app.launch()
 
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
         
