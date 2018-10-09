0f25d220dd662fcdf778fdf225b4ef3c8c3a3a44
Tapash Majumder <tapash@iterable.com>
2018-10-09 12:52:56 -0700

    Move tests to Swift.

---
+++ Tests/notification-extension-tests/NotificationExtensionSwiftTests.swift
@@ -0,0 +1,63 @@
+//
+//
+//  Created by Tapash Majumder on 10/9/18.
+//  Copyright © 2018 Iterable. All rights reserved.
+//
+
+import XCTest
+
+import UserNotifications
+
+@testable import IterableAppExtensions
+
+class NotificationExtensionSwiftTests: XCTestCase {
+    private var appExtension : ITBNotificationServiceExtension!
+    private let delay = 0.05
+    private let timeout = 15.0
+    
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        super.setUp()
+        UNUserNotificationCenter.current().setNotificationCategories([])
+        appExtension = ITBNotificationServiceExtension()
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        appExtension = nil
+        super.tearDown()
+    }
+
+    func testPushButtonWithNoType() {
+        let content = UNMutableNotificationContent()
+        let messageId = UUID().uuidString
+        content.userInfo = [
+            "itbl" : [
+                "messageId" : messageId,
+                "actionButtons" : [[
+                    "identifier" : "openAppButton",
+                    "title" : "Open App",
+                    "action" : [:],
+                ]]
+            ]
+        ]
+        
+        let request = UNNotificationRequest(identifier: "request", content: content, trigger: nil)
+        let expectation1 = expectation(description: "contentHandler is called")
+        
+        appExtension.didReceive(request) { (deliveredContent) in
+            DispatchQueue.main.asyncAfter(deadline: .now() + self.delay, execute: {
+                UNUserNotificationCenter.current().getNotificationCategories(completionHandler: { (categories) in
+                    let createdCategory = categories.first(where: {$0.identifier == messageId})
+                    XCTAssertNotNil(createdCategory)
+                    XCTAssertEqual(createdCategory!.actions.count, 1, "Number of buttons matched")
+                    XCTAssertTrue(createdCategory!.actions.first!.options.contains(.foreground), "Action is foreground")
+                    expectation1.fulfill()
+                })
+            })
+        }
+        
+        wait(for: [expectation1], timeout: timeout)
+    }
+
+}
--- Tests/ui-tests/UITests.swift
+++ Tests/ui-tests/UITests.swift
@@ -9,27 +9,23 @@ import XCTest
 
 class UITests: XCTestCase {
     private static var timeout = 60.0
-//!!!
-//    static var application: XCUIApplication = {
-//        let app = XCUIApplication()
-//        app.launch()
-//        return app
-//    }()
-//
-//    // shortcut calculated property
-//    private var app: XCUIApplication {
-//        return UITests.application
-//    }
-    private var app: XCUIApplication!
+
+    static var application: XCUIApplication = {
+        let app = XCUIApplication()
+        app.launch()
+        return app
+    }()
+
+    // shortcut calculated property
+    private var app: XCUIApplication {
+        return UITests.application
+    }
     
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
 
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
-        app = XCUIApplication()
-        app.launch()
 
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
         
