fda34e91b6112c4200a7578942f5369dee06e160
Tapash Majumder <tapash@iterable.com>
2018-11-06 15:24:58 -0800

    Add new inApp protocols and classes.

--- swift-sdk/IterableConfig.swift
+++ swift-sdk/IterableConfig.swift
@@ -35,6 +35,28 @@ import Foundation
 }
 
 /**
+ * This protocol allows you to override default behavior when new inApps arrive.
+ */
+@objc public protocol IterableInAppDelegate {
+    /**
+     * This method is called when new inApp content is available.
+     * The default behavior is to `show` if you don't override this method.
+     * - parameter content: `IterableInAppContent` object containing information regarding inApp to display
+     * - returns: Return `show` to show the inApp or `skip` to skip this.
+     */
+    func onNew(content: IterableInAppContent) -> ShowInApp
+    
+    /**
+     * This is called on more than one inApps are available.
+     * The default behavior is to call `onNew(content:)` on the list and how the first message where it returns `show`.
+     * Override this method if you want custom behavior.
+     * - parameter batch: A list of inApp contents. The default behavior is to show the first item in the list that returns `show`.
+     * - returns: An inAppContent that needs to be shown or nil if nothing needs to be shown.
+     */
+    func onNew(batch: [IterableInAppContent]) -> IterableInAppContent?
+}
+
+/**
  * Lowest level that will be logged. By default the LogLevel is set to LogLevel.info.
  */
 @objc public enum LogLevel : Int {
@@ -113,4 +135,9 @@ public class IterableConfig : NSObject {
     /// The default value is `DefaultLogDelegate`.
     /// It will log everything >= minLogLevel
     public var logDelegate: IterableLogDelegate = DefaultLogDelegate()
+    
+    /// Implement this protocol to override default inApp behavior.
+    /// By default, every single inApp will be shown as soon as it is available.
+    /// If more than 1 inApp is available, we show the first.
+    public var inAppDelegate: IterableInAppDelegate = DefaultInAppDelegate()
 }
---
+++ swift-sdk/IterableInApp.swift
@@ -0,0 +1,87 @@
+//
+//
+//  Created by Tapash Majumder on 11/5/18.
+//  Copyright © 2018 Iterable. All rights reserved.
+//
+
+import Foundation
+
+/// `show` to show the inApp otherwise `skip` to skip.
+@objc public enum ShowInApp : Int {
+    case show
+    case skip
+}
+
+
+@objc
+public protocol IterableInAppManagerProtocol {
+    /// - returns: A list of messages
+    func getMessages() -> [IterableInAppMessage]
+
+    /// - parameter content: The content to show.
+    /// - parameter consume: Set to true to consume the event from the server queue. This should be default.
+    func show(content: IterableInAppContent, consume: Bool)
+}
+
+/// By default, every single inApp will be shown as soon as it is available.
+/// If more than 1 inApp is available, we show the first showable one.
+@objcMembers
+public class DefaultInAppDelegate : IterableInAppDelegate {
+    public func onNew(content: IterableInAppContent) -> ShowInApp {
+        return .show
+    }
+    
+    public func onNew(batch: [IterableInAppContent]) -> IterableInAppContent? {
+        for content in batch {
+            if onNew(content: content) == .show {
+                return content
+            }
+        }
+
+        return nil
+    }
+}
+
+/// This class encapsulates an inApp message content such as html etc.
+@objcMembers
+public class IterableInAppContent : NSObject {
+    /// the id for the inApp message
+    public let messageId: String
+    /// Edge insets
+    public let edgeInsets: UIEdgeInsets
+    /// Background alpha setting
+    public let backgroundAlpha: Double
+    /// The html to display
+    public let html: String
+    
+    // Internal
+    init(
+        messageId: String,
+        edgeInsets: UIEdgeInsets,
+        backgroundAlpha: Double,
+        html: String) {
+        self.messageId = messageId
+        self.edgeInsets = edgeInsets
+        self.backgroundAlpha = backgroundAlpha
+        self.html = html
+    }
+}
+
+/// A message is comprised of content and whether this message was skipped.
+@objcMembers
+public class IterableInAppMessage : NSObject {
+    /// The content of the inApp message
+    let content: IterableInAppContent
+    /// Whether this message has been skipped (not shown)
+    var skipped: Bool
+    
+    init(
+        content: IterableInAppContent,
+        skipped: Bool = false
+        ) {
+        self.content = content
+        self.skipped = skipped
+    }
+}
+
+
