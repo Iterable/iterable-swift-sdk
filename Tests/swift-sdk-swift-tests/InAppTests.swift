5e2a7fee4696f033d16bad8a558a046e733ac2c1
Tapash Majumder <tapash@iterable.com>
2018-11-12 10:48:11 -0800

    Multiple messages test.

--- Tests/swift-sdk-swift-tests/InAppTests.swift
+++ Tests/swift-sdk-swift-tests/InAppTests.swift
@@ -85,6 +85,55 @@ class InAppTests: XCTestCase {
         wait(for: [expectation1], timeout: testExpectationTimeoutForInverted)
     }
 
+    func testShowInAppMultiple() {
+        let expectation1 = expectation(description: "testShowInAppMultiple")
+
+        let payload = createPayload(numMessages: 3)
+        
+        let mockInAppSynchronizer = MockInAppSynchronizer()
+        
+        let mockInAppDisplayer = MockInAppDisplayer()
+        mockInAppDisplayer.onShowCallback = {(_, _, _) in
+            expectation1.fulfill()
+        }
+        
+        IterableAPI.initializeForTesting(
+            inAppSynchronizer: mockInAppSynchronizer,
+            inAppDisplayer: mockInAppDisplayer
+        )
+        
+        mockInAppSynchronizer.mockInAppPayloadFromServer(payload)
+        
+        wait(for: [expectation1], timeout: testExpectationTimeout)
+    }
+
+    func testShowInAppMultipleOverride() {
+        let expectation1 = expectation(description: "testShowInAppMultipleOverride")
+        expectation1.isInverted = true
+        
+        let payload = createPayload(numMessages: 3)
+        
+        let mockInAppSynchronizer = MockInAppSynchronizer()
+        
+        let mockInAppDisplayer = MockInAppDisplayer()
+        mockInAppDisplayer.onShowCallback = {(_, _, _) in
+            expectation1.fulfill()
+        }
+        
+        let config = IterableConfig()
+        config.inAppDelegate = MockInAppDelegate(showInApp: .skip)
+        
+        IterableAPI.initializeForTesting(
+            config: config,
+            inAppSynchronizer: mockInAppSynchronizer,
+            inAppDisplayer: mockInAppDisplayer
+        )
+
+        mockInAppSynchronizer.mockInAppPayloadFromServer(payload)
+        
+        wait(for: [expectation1], timeout: testExpectationTimeoutForInverted)
+    }
+
     
     func testShowInAppOpenUrlByDefault() {
         let expectation1 = expectation(description: "testShowInAppByDefault")
@@ -162,5 +211,23 @@ class InAppTests: XCTestCase {
         
         wait(for: [expectation1], timeout: testExpectationTimeoutForInverted)
     }
+    
+    private func createPayload(numMessages: Int) -> [AnyHashable : Any] {
+        return [
+            "inAppMessages" : (1...numMessages).reduce(into: [[AnyHashable : Any]]()) { (result, index) in
+                result.append(createOneInAppDict(messageId: "message\(index)", campaignId: "campaign\(index)", clickUrl: "https://www.site\(index).com"))
+            }
+        ]
+    }
 
+    private func createOneInAppDict(messageId: String, campaignId: String, clickUrl: String) -> [AnyHashable : Any] {
+        return [
+            "content" : [
+                "html" : "<a href='\(clickUrl)'>Click Here</a>",
+                "inAppDisplaySettings" : ["backgroundAlpha" : 0.5, "left" : ["percentage" : 60], "right" : ["percentage" : 60], "bottom" : ["displayOption" : "AutoExpand"], "top" : ["displayOption" : "AutoExpand"]]
+            ],
+            "messageId" : "\(messageId)",
+            "campaignId" : "\(campaignId)",
+        ]
+    }
 }
