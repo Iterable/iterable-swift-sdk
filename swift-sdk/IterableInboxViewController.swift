f1fbf29a1300dad516f5cb28cfd6c1296665aa34
Tapash Majumder <tapash@iterable.com>
2019-04-16 15:49:01 -0700

    Now with delete.

--- swift-sdk/IterableInboxViewController.swift
+++ swift-sdk/IterableInboxViewController.swift
@@ -56,40 +56,22 @@ open class IterableInboxViewController: UITableViewController {
         return cell
     }
 
-    /*
     // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+    override open func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
 
-    /*
     // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+    override open func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
+            let iterableMessage = viewModels[indexPath.row].iterableMessage
+            viewModels.remove(at: indexPath.row)
+            IterableAPI.inAppManager.remove(message: iterableMessage)
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
     
     override open func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let iterableMessage = viewModels[indexPath.row].iterableMessage
@@ -117,15 +99,15 @@ open class IterableInboxViewController: UITableViewController {
     }
     
     private func refreshMessages() {
-        viewModels.removeAll()
-        IterableAPI.inAppManager.getInboxMessages().forEach {
-            viewModels.append(InboxMessageViewModel.from(message: $0))
-        }
-
-        let unreadCount = IterableAPI.inAppManager.getUnreadInboxMessagesCount()
-        let badgeValue = unreadCount == 0 ? nil : "\(unreadCount)"
-
         DispatchQueue.main.async { [weak self] in
+            self?.viewModels.removeAll()
+            IterableAPI.inAppManager.getInboxMessages().forEach {
+                self?.viewModels.append(InboxMessageViewModel.from(message: $0))
+            }
+            
+            let unreadCount = IterableAPI.inAppManager.getUnreadInboxMessagesCount()
+            let badgeValue = unreadCount == 0 ? nil : "\(unreadCount)"
+            
             self?.navigationController?.tabBarItem?.badgeValue = badgeValue
             self?.tableView.reloadData()
         }
