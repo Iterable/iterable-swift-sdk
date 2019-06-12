dd98996883a12f013a91e558ce3b531383d58d55
Tapash Majumder <tapash@iterable.com>
2019-06-12 12:44:11 -0700

    Introduce IterableInboxViewControllerViewModel

---
+++ swift-sdk/Internal/InboxViewControllerViewModel.swift
@@ -0,0 +1,120 @@
+//
+//
+//  Created by Tapash Majumder on 6/12/19.
+//  Copyright © 2019 Iterable. All rights reserved.
+//
+
+import Foundation
+
+protocol InboxViewControllerViewModelDelegate : class {
+    // All these methods should be called on the main thread
+    func onViewModelChanged(diff: [SectionedDiffStep<Int, InboxMessageViewModel>])
+    func onImageLoaded(forRow row: Int)
+}
+
+protocol InboxViewControllerViewModelProtocol {
+    var delegate: InboxViewControllerViewModelDelegate? { get set}
+    var numMessages: Int { get }
+    var unreadCount: Int { get }
+    func message(atRow row: Int) -> InboxMessageViewModel
+    func remove(atRow row: Int)
+    func refresh() -> Future<Bool, Error> // Talks to the server and refreshes
+    // this works hand in hand with listener.onViewModelChanged.
+    // Internal model can't be changed until the view begins update.
+    func beganUpdates()
+}
+
+class InboxViewControllerViewModel : InboxViewControllerViewModelProtocol {
+    weak var delegate: InboxViewControllerViewModelDelegate?
+    
+    init() {
+        ITBInfo()
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(onInboxChanged(notification:)), name: .iterableInboxChanged, object: nil)
+    }
+    
+    deinit {
+        ITBInfo()
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    var numMessages: Int {
+        return messages.count
+    }
+    
+    var unreadCount: Int {
+        return messages.filter { $0.read == false }.count
+    }
+    
+    func message(atRow row: Int) -> InboxMessageViewModel {
+        let message = messages[row]
+        loadImageIfNecessary(message)
+        return message
+    }
+    
+    func remove(atRow row: Int) {
+        IterableAPI.inAppManager.remove(message: messages[row].iterableMessage)
+    }
+    
+    func refresh() -> Future<Bool, Error> {
+        guard let inAppManager = IterableAPI.inAppManager as? InAppManager else {
+            return Promise(error: IterableError.general(description: "Did not find inAppManager"))
+        }
+        
+        return inAppManager.scheduleSync()
+    }
+    
+    func beganUpdates() {
+        messages = newMessages
+    }
+    
+    private func loadImageIfNecessary(_ message: InboxMessageViewModel) {
+        guard let imageUrlString = message.imageUrl, let url = URL(string: imageUrlString) else {
+            return
+        }
+        
+        if message.imageData == nil {
+            loadImage(forMessageId: message.iterableMessage.messageId, fromUrl: url)
+        }
+    }
+    
+    private func loadImage(forMessageId messageId: String, fromUrl url: URL) {
+        if let networkSession = IterableAPI.internalImplementation?.networkSession {
+            NetworkHelper.getData(fromUrl: url, usingSession: networkSession).onSuccess {[weak self] in
+                self?.setImageData($0, forMessageId: messageId)
+                }.onError {
+                    ITBError($0.localizedDescription)
+            }
+        }
+    }
+    
+    private func setImageData(_ data: Data, forMessageId messageId: String) {
+        guard let row = messages.firstIndex (where: { $0.iterableMessage.messageId == messageId }) else {
+            return
+        }
+        let message = messages[row]
+        message.imageData = data
+        
+        self.delegate?.onImageLoaded(forRow: row)
+    }
+    
+    
+    @objc private func onInboxChanged(notification: NSNotification) {
+        ITBInfo()
+        
+        let oldSectionedValues = AbstractDiffCalculator<Int, InboxMessageViewModel>.buildSectionedValues(values: messages, sectionIndex: 0)
+        newMessages = IterableAPI.inAppManager.getInboxMessages().map { InboxMessageViewModel(message: $0) }
+        let newSectionedValues = AbstractDiffCalculator<Int, InboxMessageViewModel>.buildSectionedValues(values: newMessages, sectionIndex: 0)
+        
+        let diff = Dwifft.diff(lhs: oldSectionedValues, rhs: newSectionedValues)
+        if (diff.count > 0) {
+            DispatchQueue.main.async { [weak self] in
+                self?.delegate?.onViewModelChanged(diff: diff)
+            }
+        }
+    }
+    
+    private var messages = [InboxMessageViewModel]()
+    private var newMessages = [InboxMessageViewModel]()
+}
+
--- swift-sdk/IterableInboxViewController.swift
+++ swift-sdk/IterableInboxViewController.swift
@@ -14,26 +14,29 @@ open class IterableInboxViewController: UITableViewController {
     /// that the xib is present in the main bundle.
     @IBInspectable public var cellNibName: String? = nil
     
+    /// You can change insertion/deletion animations here.
+    public var insertionAnimation = UITableView.RowAnimation.automatic, deletionAnimation = UITableView.RowAnimation.automatic
+
     // MARK: Initializers
     public override init(style: UITableView.Style) {
         ITBInfo()
+        self.viewModel = InboxViewControllerViewModel()
         super.init(style: style)
-     
-        setup()
+        viewModel.delegate = self
     }
     
     public required init?(coder aDecoder: NSCoder) {
         ITBInfo()
+        self.viewModel = InboxViewControllerViewModel()
         super.init(coder: aDecoder)
-        
-        setup()
+        viewModel.delegate = self
     }
     
     public override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
         ITBInfo()
+        self.viewModel = InboxViewControllerViewModel()
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
-        
-        setup()
+        viewModel.delegate = self
     }
     
     override open func viewDidLoad() {
@@ -63,7 +66,7 @@ open class IterableInboxViewController: UITableViewController {
     }
 
     override open func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return diffCalculator?.rows.count ?? 0
+        return viewModel.numMessages
     }
 
     override open func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -71,9 +74,7 @@ open class IterableInboxViewController: UITableViewController {
             fatalError("Please make sure that an the nib: \(cellNibName!) is present in the main bundle")
         }
 
-        if let viewModel = diffCalculator?.rows[indexPath.row] {
-            configure(cell: cell, forViewModel: viewModel)
-        }
+        configure(cell: cell, forMessage:viewModel.message(atRow: indexPath.row))
 
         return cell
     }
@@ -88,37 +89,25 @@ open class IterableInboxViewController: UITableViewController {
     override open func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
-            guard let iterableMessage = diffCalculator?.rows[indexPath.row].iterableMessage else {
-                return
-            }
-            IterableAPI.inAppManager.remove(message: iterableMessage)
+            viewModel.remove(atRow: indexPath.row)
         }
     }
     
     override open func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        guard let iterableMessage = diffCalculator?.rows[indexPath.row].iterableMessage else {
-            return
-        }
+        let iterableMessage = viewModel.message(atRow: indexPath.row).iterableMessage
+        
         if let viewController = IterableAPI.inAppManager.createInboxMessageViewController(for: iterableMessage) {
             IterableAPI.inAppManager.set(read: true, forMessage: iterableMessage)
             navigationController?.pushViewController(viewController, animated: true)
         }
     }
 
+    var viewModel: InboxViewControllerViewModelProtocol
+    
     private let iterableCellNibName = "IterableInboxCell"
     
-    private lazy var diffCalculator: SingleSectionTableViewDiffCalculator<InboxMessageViewModel>? = {
-        SingleSectionTableViewDiffCalculator(tableView: self.tableView)
-    }()
-    
-    private func setup() {
-        refreshMessages()
-        NotificationCenter.default.addObserver(self, selector: #selector(onInboxChanged(notification:)), name: .iterableInboxChanged, object: nil)
-    }
-
     deinit {
         ITBInfo()
-        NotificationCenter.default.removeObserver(self)
     }
     
     private func registerTableViewCell() {
@@ -129,92 +118,84 @@ open class IterableInboxViewController: UITableViewController {
         tableView.register(nib, forCellReuseIdentifier: "inboxCell")
     }
     
-    @objc private func onInboxChanged(notification: NSNotification) {
-        ITBInfo()
-        refreshMessages()
-    }
-    
-    private func refreshMessages() {
-        ITBInfo()
-        DispatchQueue.main.async { [weak self] in
-            self?.updateUnreadBadgeCount()
-            self?.diffCalculator?.rows = IterableAPI.inAppManager.getInboxMessages().map { InboxMessageViewModel(message: $0) }
-        }
-    }
-    
-    private func updateUnreadBadgeCount() {
-        let unreadCount = IterableAPI.inAppManager.getUnreadInboxMessagesCount()
-        let badgeValue = unreadCount == 0 ? nil : "\(unreadCount)"
-        navigationController?.tabBarItem?.badgeValue = badgeValue
-    }
-    
     @available(iOS 10.0, *)
     @objc private func handleRefreshControl() {
         ITBInfo()
-        
-        if let inAppManager = IterableAPI.inAppManager as? InAppManager {
-            _ = inAppManager.scheduleSync()
-        }
+
+        _ = viewModel.refresh()
         
         tableView.refreshControl?.endRefreshing()
     }
     
-    private func configure(cell: IterableInboxCell, forViewModel viewModel: InboxMessageViewModel) {
-        cell.titleLbl?.text = viewModel.title
-        cell.subtitleLbl?.text = viewModel.subtitle
-        cell.unreadCircleView?.isHidden = viewModel.read
+    private func configure(cell: IterableInboxCell, forMessage message: InboxMessageViewModel) {
+        cell.titleLbl?.text = message.title
+        cell.subtitleLbl?.text = message.subtitle
+        cell.unreadCircleView?.isHidden = message.read
         
         cell.iconImageView?.clipsToBounds = true
 
-        if let imageUrlString = viewModel.imageUrl, let url = URL(string: imageUrlString) {
+        if message.hasValidImageUrl() {
             cell.iconContainerView?.isHidden = false
             cell.iconImageView?.isHidden = false
 
-            if let data = viewModel.imageData {
+            if let data = message.imageData {
                 cell.iconImageView?.backgroundColor = nil
                 cell.iconImageView?.image = UIImage(data: data)
             } else {
                 cell.iconImageView?.backgroundColor = UIColor(hex: "EEEEEE") // loading image
                 cell.iconImageView?.image = nil
-                loadImage(forMessageId: viewModel.iterableMessage.messageId, fromUrl: url)
-            }
+             }
         } else {
             cell.iconContainerView?.isHidden = true
             cell.iconImageView?.isHidden = true
         }
         
-        if let createdAt = viewModel.createdAt {
+        if let createdAt = message.createdAt {
             cell.createdAtLbl?.isHidden = false
             cell.createdAtLbl?.text = IterableInboxViewController.displayValue(forTime: createdAt)
         } else {
             cell.createdAtLbl?.isHidden = true
         }
     }
-
-    private func loadImage(forMessageId messageId: String, fromUrl url: URL) {
-        if let networkSession = IterableAPI.internalImplementation?.networkSession {
-            NetworkHelper.getData(fromUrl: url, usingSession: networkSession).onSuccess {[weak self] in
-                self?.updateCell(forMessageId: messageId, withImageData: $0)
-            }.onError {
-                ITBError($0.localizedDescription)
-            }
-        }
-    }
-    
-    private func updateCell(forMessageId messageId: String, withImageData data: Data) {
-        guard var viewModels = diffCalculator?.rows else {
-            return
-        }
-        guard let row = viewModels.firstIndex (where: { $0.iterableMessage.messageId == messageId }) else {
-            return
-        }
-        let viewModel = viewModels[row]
-        viewModel.imageData = data
-
-        tableView.reloadRows(at: [IndexPath(row: row, section: 0)], with: .automatic)
-    }
     
     private static func displayValue(forTime date: Date) -> String {
         return DateFormatter.localizedString(from: date, dateStyle: .medium, timeStyle: .short)
     }
+}
+
+extension IterableInboxViewController : InboxViewControllerViewModelDelegate {
+    // Must be called on Main Thread
+    func onViewModelChanged(diff: [SectionedDiffStep<Int, InboxMessageViewModel>]) {
+        ITBInfo()
+
+        updateTableView(diff: diff)
+        updateUnreadBadgeCount()
+    }
+
+    // Must be called on Main Thread
+    func onImageLoaded(forRow row: Int) {
+        ITBInfo()
+        tableView.reloadRows(at: [IndexPath(row: row, section: 0)], with: .automatic)
+    }
+
+    private func updateUnreadBadgeCount() {
+        let unreadCount = viewModel.unreadCount
+        let badgeValue = unreadCount == 0 ? nil : "\(unreadCount)"
+        navigationController?.tabBarItem?.badgeValue = badgeValue
+    }
+    
+    private func updateTableView(diff: [SectionedDiffStep<Int, InboxMessageViewModel>]) {
+        tableView.beginUpdates()
+        viewModel.beganUpdates()
+        for result in diff {
+            switch result {
+            case let .delete(section, row, _): tableView.deleteRows(at: [IndexPath(row: row, section: section)], with: self.deletionAnimation)
+            case let .insert(section, row, _): tableView.insertRows(at: [IndexPath(row: row, section: section)], with: self.insertionAnimation)
+            case let .sectionDelete(section, _): tableView.deleteSections(IndexSet(integer: section), with: self.deletionAnimation)
+            case let .sectionInsert(section, _): tableView.insertSections(IndexSet(integer: section), with: self.insertionAnimation)
+            }
+        }
+        tableView.endUpdates()
+    }
+    
 }
