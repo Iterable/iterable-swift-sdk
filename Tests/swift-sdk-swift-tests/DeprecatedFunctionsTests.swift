6eb0b89b339436726a15d7ae71e83b6e44e1b168
Jay Kim <jay@nullavenue.com>
2019-10-08 15:43:48 -0700

    Create new testing file, reorganize project

---
+++ Tests/swift-sdk-swift-tests/DeprecatedFunctionsTests.swift
@@ -0,0 +1,15 @@
+//
+//  DeprecatedFunctionsTests.swift
+//  swift-sdk-swift-tests
+//
+//  Created by Jay Kim on 10/8/19.
+//  Copyright © 2019 Iterable. All rights reserved.
+//
+
+import XCTest
+
+@testable import IterableSDK
+
+class DeprecatedFunctionsTests: XCTestCase {
+    
+}
