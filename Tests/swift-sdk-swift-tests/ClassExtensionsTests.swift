8f7f5f681d6d97fe7950096b56fed664eae6d6f8
Tapash Majumder <tapash@iterable.com>
2019-10-08 14:31:38 +0530

    Test for token to hex conversion.

--- Tests/swift-sdk-swift-tests/ClassExtensionsTests.swift
+++ Tests/swift-sdk-swift-tests/ClassExtensionsTests.swift
@@ -10,6 +10,8 @@ import XCTest
 
 @testable import IterableSDK
 
+typealias Byte = UInt8
+
 class ClassExtensionsTests: XCTestCase {
     func testUIColorInit() {
         let blackColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1)
@@ -20,5 +22,45 @@ class ClassExtensionsTests: XCTestCase {
         }
         
         XCTAssertEqual(hexColor, blackColor)
+    }
+    
+    func testHexConversion() {
+        10.times {
+            let token = generateToken()
+            let hex = token.hexString()
+            XCTAssertEqual(token, data(fromHexString: hex))
+        }
+    }
+    
+    private func generateRandomInt(max: Int) -> Int {
+        return Int(arc4random_uniform(UInt32(max)))
+    }
+    
+    private func generateToken() -> Data {
+        var byteArray = [Byte]()
+        
+        32.times {
+            byteArray.append(Byte(generateRandomInt(max: 256)))
+        }
+        
+        return Data(byteArray)
+    }
+    
+    // maps a hex char to corresponding digit
+    // e.g., a -> 10, b -> 11
+    private func hexCharToDigit(_ hexChar: Character) -> Byte? {
+        let digits = Array("0123456789abcdef")
+        return digits.firstIndex(of: hexChar).map { Byte($0) }
+    }
+    
+    private func hexCharsToByte(c1: Character, c2: Character) -> (Byte)? {
+        guard let d1 = hexCharToDigit(c1), let d2 = hexCharToDigit(c2) else {
+            return nil
+        }
+        return d1 * 16 + d2
+    }
+    
+    private func data(fromHexString hexString: String) -> Data {
+        return Data(Array(hexString).take(2).map { hexCharsToByte(c1: $0[0], c2: $0[1]) }.compactMap { $0 })
     }
 }
