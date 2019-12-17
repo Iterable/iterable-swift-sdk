09a601d53bdaf0f4e6c8e89f70bb536d8f9badbb
Tapash Majumder <tapash@iterable.com>
2019-12-17 18:54:10 +0530

    Use MockNetworkSession. Add tests for non-existing image.

--- Tests/swift-sdk-swift-tests/InboxViewControllerViewModelTests.swift
+++ Tests/swift-sdk-swift-tests/InboxViewControllerViewModelTests.swift
@@ -286,6 +286,7 @@ class InboxViewControllerViewModelTests: XCTestCase {
         let expectation1 = expectation(description: "testImageLoadingForExistingImage")
         
         let model = InboxViewControllerViewModel()
+        
         let mockView = MockViewModelView()
         mockView.onImageLoadedCallback = { indexPath in
             XCTAssertNotNil(model.message(atIndexPath: indexPath).imageData)
@@ -293,9 +294,10 @@ class InboxViewControllerViewModelTests: XCTestCase {
         }
         model.view = mockView
         
+        let mockNetworkSession = MockNetworkSession(statusCode: 200, data: Data(repeating: 0, count: 100))
         let fetcher = MockInAppFetcher()
-        
         IterableAPI.initializeForTesting(
+            networkSession: mockNetworkSession,
             inAppFetcher: fetcher
         )
         
@@ -319,6 +321,48 @@ class InboxViewControllerViewModelTests: XCTestCase {
         }
         
         wait(for: [expectation1], timeout: testExpectationTimeout)
+    }
+    
+    func testImageLoadingForNonExistingImage() {
+        let expectation1 = expectation(description: "testImageLoadingForNonExistingImage")
+        expectation1.isInverted = true
+        
+        let model = InboxViewControllerViewModel()
+        
+        let mockView = MockViewModelView()
+        mockView.onImageLoadedCallback = { indexPath in
+            XCTAssertNotNil(model.message(atIndexPath: indexPath).imageData)
+            expectation1.fulfill()
+        }
+        model.view = mockView
+        
+        let mockNetworkSession = MockNetworkSession(statusCode: 404, data: nil)
+        let fetcher = MockInAppFetcher()
+        IterableAPI.initializeForTesting(
+            networkSession: mockNetworkSession,
+            inAppFetcher: fetcher
+        )
+        
+        let imageLocation = "file:///something.png"
+        
+        let messages = [
+            IterableInAppMessage(messageId: "message1",
+                                 campaignId: "",
+                                 trigger: IterableInAppTrigger(dict: [JsonKey.InApp.type: "never"]),
+                                 content: IterableHtmlInAppContent(edgeInsets: .zero, backgroundAlpha: 0.0, html: ""),
+                                 saveToInbox: true,
+                                 inboxMetadata: IterableInboxMetadata(title: "inbox title", subtitle: "inbox subtitle", icon: imageLocation),
+                                 customPayload: ["messageType": "transactional"]),
+        ]
+        fetcher.mockMessagesAvailableFromServer(messages: messages)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            model.beganUpdates()
+            XCTAssertEqual(model.numRows(in: 0), 1)
+            XCTAssertEqual(model.message(atIndexPath: IndexPath(row: 0, section: 0)).iterableMessage.messageId, "message1")
+        }
+        
+        wait(for: [expectation1], timeout: 5.0)
     }
     
     private class MockViewModelView: InboxViewControllerViewModelView {
