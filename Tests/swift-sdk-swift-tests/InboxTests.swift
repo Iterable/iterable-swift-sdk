e9031cb527fde4aa3a16b40b1c98076031f330a7
Jay Kim <jay@nullavenue.com>
2020-01-13 18:08:26 -0800

    Add test for receiving an already read message

--- Tests/swift-sdk-swift-tests/InboxTests.swift
+++ Tests/swift-sdk-swift-tests/InboxTests.swift
@@ -129,6 +129,37 @@ class InboxTests: XCTestCase {
         wait(for: [expectation1], timeout: testExpectationTimeout)
     }
     
+    func testReceiveReadMessage() {
+        let expectation1 = expectation(description: "testReceiveReadMessage")
+        let mockInAppFetcher = MockInAppFetcher()
+        
+        IterableAPI.initializeForTesting(inAppFetcher: mockInAppFetcher)
+        
+        let payload = """
+            {"inAppMessages": [{
+                "saveToInbox": true,
+                "content": {"contentType": "html", "inAppDisplaySettings": {"bottom": {"displayOption": "AutoExpand"}, "backgroundAlpha": 0.5, "left": {"percentage": 60}, "right": {"percentage": 60}, "top": {"displayOption": "AutoExpand"}}, "html": "<a href=\'https://www.site2.com\'>Click Here</a>"},
+                "trigger": {"type": "never"},
+                "messageId": "23fuih4evr0kn",
+                "campaignId": "2308",
+                "read": true
+                }]
+            }
+        """.toJsonDict()
+        
+        mockInAppFetcher.mockInAppPayloadFromServer(payload).onSuccess { _ in
+            let messages = IterableAPI.inAppManager.getInboxMessages()
+            
+            if let msg = messages.first, msg.read {
+                expectation1.fulfill()
+            } else {
+                XCTFail()
+            }
+        }
+        
+        wait(for: [expectation1], timeout: testExpectationTimeout)
+    }
+    
     func testRemove() {
         let expectation1 = expectation(description: "testRemove")
         let mockInAppFetcher = MockInAppFetcher()
