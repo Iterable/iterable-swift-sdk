36da579275e4acc2b71ffaa07352d592a88962b9
Tapash Majumder <tapash@iterable.com>
2020-01-14 10:14:16 +0530

    First version created by Xcode.

---
+++ sample-apps/inbox-customization/inbox-customization/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  inbox-customization
+//
+//  Created by Tapash Majumder on 1/14/20.
+//  Copyright © 2020 Iterable. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
