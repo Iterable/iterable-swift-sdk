51e71b53cf08fe9158b46fc6e2681da412558e66
Tapash Majumder <tapash@iterable.com>
2020-01-20 14:50:53 +0530

    Inbox with multiple cell types separated into different directory.

---
+++ sample-apps/inbox-customization/inbox-customization/Inbox With Multiple Cell Types/Inbox With Multiple Cell Types.swift
@@ -0,0 +1,9 @@
+//
+//  Inbox With Multiple Cell Types.swift
+//  inbox-customization
+//
+//  Created by Tapash Majumder on 1/20/20.
+//  Copyright © 2020 Iterable. All rights reserved.
+//
+
+import Foundation
