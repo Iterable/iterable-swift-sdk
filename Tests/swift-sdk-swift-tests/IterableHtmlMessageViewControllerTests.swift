05cfc9914faf539e90d6f7c006588154a4ad6364
Tapash Majumder <tapash@iterable.com>
2020-03-10 01:03:35 +0530

    Fix unit test

--- Tests/swift-sdk-swift-tests/IterableHtmlMessageViewControllerTests.swift
+++ Tests/swift-sdk-swift-tests/IterableHtmlMessageViewControllerTests.swift
@@ -21,30 +21,51 @@ class IterableHtmlMessageViewControllerTests: XCTestCase {
         super.tearDown()
     }
     
-    func testWebViewWidth() {
+    func testWebViewPositioning() {
         IterableAPI.initializeForTesting()
         struct MockViewCalculations: ViewCalculationsProtocol {
             func width(for _: UIView) -> CGFloat {
                 return 1234.0
             }
         }
+        class MockWebView: WebViewProtocol {
+            let view: UIView = UIView()
+            
+            func loadHTMLString(_: String, baseURL _: URL?) -> WKNavigation? {
+                return nil
+            }
+            
+            func set(position: ViewPosition) {
+                self.position = position
+                view.frame.size.width = position.width
+                view.frame.size.height = position.height
+                view.center = position.center
+            }
+            
+            func set(navigationDelegate _: WKNavigationDelegate?) {}
+            
+            func evaluateJavaScript(_: String, completionHandler: ((Any?, Error?) -> Void)?) {
+                completionHandler?(CGFloat(200.0), nil)
+            }
+            
+            func layoutSubviews() {}
+            
+            var position: ViewPosition?
+        }
         struct MockInjectedDependencyModule: InjectedDependencyModuleProtocol {
+            static let shared = MockInjectedDependencyModule()
+            
             let viewCalculations: ViewCalculationsProtocol = MockViewCalculations()
             
-            var webView: WebViewProtocol {
-                let webView = WKWebView(frame: .zero)
-                webView.scrollView.bounces = false
-                webView.isOpaque = false
-                webView.backgroundColor = UIColor.clear
-                return webView as WebViewProtocol
-            }
-        }
-        InjectedDependencies.shared.set {
-            MockInjectedDependencyModule() as InjectedDependencyModuleProtocol
+            let webView: WebViewProtocol = MockWebView()
+            
+            init() {}
         }
         
-        let xpectation1 = expectation(description: "testWebViewWidth")
-        xpectation1.isInverted = true // won't be called, used just for waiting
+        InjectedDependencies.shared.set {
+            MockInjectedDependencyModule.shared as InjectedDependencyModuleProtocol
+        }
+        
         let viewController = IterableHtmlMessageViewController.create(parameters: IterableHtmlMessageViewController.Parameters(html: "",
                                                                                                                                padding: UIEdgeInsets(top: 0, left: 0, bottom: -1, right: 0),
                                                                                                                                messageMetadata: nil,
@@ -52,7 +73,11 @@ class IterableHtmlMessageViewControllerTests: XCTestCase {
                                                                                                                                inboxSessionId: nil)).viewController
         viewController.loadView()
         viewController.viewDidLayoutSubviews()
-        wait(for: [xpectation1], timeout: 5.0)
-        XCTAssertEqual(viewController.webView.view.frame.width, 1234.0)
+        
+        let mockWebView = MockInjectedDependencyModule.shared.webView as! MockWebView
+        XCTAssertEqual(mockWebView.view.frame.width, 1234.0)
+        XCTAssertEqual(mockWebView.view.frame.height, 200.0)
+        XCTAssertEqual(mockWebView.view.center.x, 617.0)
+        XCTAssertEqual(mockWebView.view.center.y, 100.0)
     }
 }
