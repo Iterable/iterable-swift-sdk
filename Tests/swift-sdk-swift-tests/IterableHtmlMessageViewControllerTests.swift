916d5fe0972ce6559e58835e8e7dd488376cf3fe
Tapash Majumder <tapash@iterable.com>
2020-03-10 23:04:50 +0530

    Write tests for web view positioning.

--- Tests/swift-sdk-swift-tests/IterableHtmlMessageViewControllerTests.swift
+++ Tests/swift-sdk-swift-tests/IterableHtmlMessageViewControllerTests.swift
@@ -21,63 +21,101 @@ class IterableHtmlMessageViewControllerTests: XCTestCase {
         super.tearDown()
     }
     
-    func testWebViewPositioning() {
+    func testWebViewTopPositioning() {
         IterableAPI.initializeForTesting()
-        struct MockViewCalculations: ViewCalculationsProtocol {
-            func width(for _: UIView) -> CGFloat {
-                return 1234.0
-            }
-        }
-        class MockWebView: WebViewProtocol {
-            let view: UIView = UIView()
-            
-            func loadHTMLString(_: String, baseURL _: URL?) -> WKNavigation? {
-                return nil
-            }
-            
-            func set(position: ViewPosition) {
-                self.position = position
-                view.frame.size.width = position.width
-                view.frame.size.height = position.height
-                view.center = position.center
-            }
-            
-            func set(navigationDelegate _: WKNavigationDelegate?) {}
-            
-            func evaluateJavaScript(_: String, completionHandler: ((Any?, Error?) -> Void)?) {
-                completionHandler?(CGFloat(200.0), nil)
-            }
-            
-            func layoutSubviews() {}
-            
-            var position: ViewPosition?
-        }
-        struct MockInjectedDependencyModule: InjectedDependencyModuleProtocol {
-            static let shared = MockInjectedDependencyModule()
-            
-            let viewCalculations: ViewCalculationsProtocol = MockViewCalculations()
-            
-            let webView: WebViewProtocol = MockWebView()
-            
-            init() {}
-        }
+        checkPositioning(viewPosition: ViewPosition(width: 1234, height: 400, center: CGPoint(x: 617.0, y: 200.0)),
+                         safeAreaInsets: .zero,
+                         inAppHeight: 200,
+                         messageLocation: .top,
+                         expectedWebViewPosition: ViewPosition(width: 1234, height: 200, center: CGPoint(x: 617.0, y: 100.0)))
+    }
+    
+    func testWebViewBottomPositioning() {
+        IterableAPI.initializeForTesting()
+        checkPositioning(viewPosition: ViewPosition(width: 1234, height: 400, center: CGPoint(x: 617.0, y: 200.0)),
+                         safeAreaInsets: .zero,
+                         inAppHeight: 200,
+                         messageLocation: .bottom,
+                         expectedWebViewPosition: ViewPosition(width: 1234, height: 200, center: CGPoint(x: 617.0, y: 300.0)))
+    }
+    
+    func testWebViewCenterPositioning() {
+        IterableAPI.initializeForTesting()
+        checkPositioning(viewPosition: ViewPosition(width: 1234, height: 400, center: CGPoint(x: 617.0, y: 200.0)),
+                         safeAreaInsets: .zero,
+                         inAppHeight: 200,
+                         messageLocation: .center,
+                         expectedWebViewPosition: ViewPosition(width: 1234, height: 200, center: CGPoint(x: 617.0, y: 200.0)))
+    }
+    
+    func testWebViewFullPositioning() {
+        IterableAPI.initializeForTesting()
+        checkPositioning(viewPosition: ViewPosition(width: 1234, height: 400, center: CGPoint(x: 617.0, y: 200.0)),
+                         safeAreaInsets: .zero,
+                         inAppHeight: 200,
+                         messageLocation: .full,
+                         expectedWebViewPosition: ViewPosition(width: 1234, height: 400, center: CGPoint(x: 617.0, y: 200.0)))
+    }
+    
+    func testWebViewTopPositioningWithSafeAreaInsets() {
+        IterableAPI.initializeForTesting()
+        checkPositioning(viewPosition: ViewPosition(width: 1234, height: 400, center: CGPoint(x: 617.0, y: 200.0)),
+                         safeAreaInsets: UIEdgeInsets(top: 25, left: 0, bottom: 30, right: 0),
+                         inAppHeight: 200,
+                         messageLocation: .top,
+                         expectedWebViewPosition: ViewPosition(width: 1234, height: 200, center: CGPoint(x: 617.0, y: 125.0)))
+    }
+    
+    func testWebViewBottomPositioningWithSafeAreaInsets() {
+        IterableAPI.initializeForTesting()
+        checkPositioning(viewPosition: ViewPosition(width: 1234, height: 400, center: CGPoint(x: 617.0, y: 200.0)),
+                         safeAreaInsets: UIEdgeInsets(top: 25, left: 0, bottom: 30, right: 0),
+                         inAppHeight: 200,
+                         messageLocation: .bottom,
+                         expectedWebViewPosition: ViewPosition(width: 1234, height: 200, center: CGPoint(x: 617.0, y: 270.0)))
+    }
+    
+    private func checkPositioning(viewPosition: ViewPosition,
+                                  safeAreaInsets: UIEdgeInsets,
+                                  inAppHeight: CGFloat,
+                                  messageLocation: IterableMessageLocation,
+                                  expectedWebViewPosition: ViewPosition) {
+        let viewCalculations = MockViewCalculations(viewPosition: viewPosition, safeAreaInsets: safeAreaInsets)
+        let webView = MockWebView(height: inAppHeight)
+        let dependencyModule = MockInjectedDependencyModule(viewCalculations: viewCalculations, webView: webView)
         
         InjectedDependencies.shared.set {
-            MockInjectedDependencyModule.shared as InjectedDependencyModuleProtocol
+            dependencyModule as InjectedDependencyModuleProtocol
         }
         
         let viewController = IterableHtmlMessageViewController.create(parameters: IterableHtmlMessageViewController.Parameters(html: "",
-                                                                                                                               padding: UIEdgeInsets(top: 0, left: 0, bottom: -1, right: 0),
+                                                                                                                               padding: padding(from: messageLocation),
                                                                                                                                messageMetadata: nil,
                                                                                                                                isModal: true,
                                                                                                                                inboxSessionId: nil)).viewController
         viewController.loadView()
         viewController.viewDidLayoutSubviews()
         
-        let mockWebView = MockInjectedDependencyModule.shared.webView as! MockWebView
-        XCTAssertEqual(mockWebView.view.frame.width, 1234.0)
-        XCTAssertEqual(mockWebView.view.frame.height, 200.0)
-        XCTAssertEqual(mockWebView.view.center.x, 617.0)
-        XCTAssertEqual(mockWebView.view.center.y, 100.0)
+        checkPosition(for: webView.view, expectedPosition: expectedWebViewPosition)
+    }
+    
+    private func checkPosition(for view: UIView, expectedPosition: ViewPosition) {
+        XCTAssertEqual(view.frame.width, expectedPosition.width)
+        XCTAssertEqual(view.frame.height, expectedPosition.height)
+        XCTAssertEqual(view.center.x, expectedPosition.center.x)
+        XCTAssertEqual(view.center.y, expectedPosition.center.y)
+    }
+    
+    private func padding(from messageLocation: IterableMessageLocation) -> UIEdgeInsets {
+        switch messageLocation {
+        case .top:
+            return UIEdgeInsets(top: 0, left: 0, bottom: -1, right: 0)
+        case .bottom:
+            return UIEdgeInsets(top: -1, left: 0, bottom: 0, right: 0)
+        case .center:
+            return UIEdgeInsets(top: -1, left: 0, bottom: -1, right: 0)
+        case .full:
+            return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        }
     }
 }
