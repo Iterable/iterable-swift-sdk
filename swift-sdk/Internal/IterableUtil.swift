a48c89c6274688f44fda020a2edce889bf0eb973
Tapash Majumder <tapash@iterable.com>
2020-05-13 14:07:33 +0530

    Fix. RootViewController can't be obtained from AppDelegate for iOS 13.

--- swift-sdk/Internal/IterableUtil.swift
+++ swift-sdk/Internal/IterableUtil.swift
@@ -15,7 +15,11 @@ enum IterableResult<T, E> {
 
 @objc public final class IterableUtil: NSObject {
     static var rootViewController: UIViewController? {
-        return UIApplication.shared.delegate?.window??.rootViewController
+        if let rootViewController = UIApplication.shared.delegate?.window??.rootViewController {
+            return rootViewController
+        } else {
+            return UIApplication.shared.windows.first?.rootViewController
+        }
     }
     
     static func trim(string: String) -> String {
