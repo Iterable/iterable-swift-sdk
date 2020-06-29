66677997f6fe64e8c5b0719759faddf32d5d0905
Tapash Majumder <tapash@iterable.com>
2020-06-29 21:37:57 +0530

    1. Add endpoint-tests target.
    2. Disable running swiftformat for now.

---
+++ tests/endpoint-tests/EndpointTests.swift
@@ -0,0 +1,28 @@
+//
+//  Created by Tapash Majumder on 6/29/20.
+//  Copyright © 2020 Iterable. All rights reserved.
+//
+
+import XCTest
+
+class EndpointTests: XCTestCase {
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+}
