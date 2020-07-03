380b14b232ec3c5743a512933463c972a254bb28
Tapash Majumder <tapash@iterable.com>
2020-07-03 10:56:23 +0530

    Add tests for send/consume inApp. Misc supporting functions.

--- tests/endpoint-tests/IterableAPISupport.swift
+++ tests/endpoint-tests/IterableAPISupport.swift
@@ -18,9 +18,30 @@ struct IterableAPISupport {
         return NetworkHelper.sendRequest(urlRequest, usingSession: urlSession)
     }
     
+    static func sendInApp(to email: String, withCampaignId campaignId: Int) -> Future<SendRequestValue, SendRequestError> {
+        let body: [String: Any] = [
+            Key.inAppRecipientEmail: email,
+            Key.inAppCampaignId: campaignId
+        ]
+        let iterablePostRequest = PostRequest(path: Path.inAppTarget,
+                                              args: nil,
+                                              body: body)
+        guard let urlRequest = createPostRequest(iterablePostRequest: iterablePostRequest) else {
+            return SendRequestError.createErroredFuture(reason: "could not create post request")
+        }
+        
+        return NetworkHelper.sendRequest(urlRequest, usingSession: urlSession)
+    }
+
     private enum Path {
         static let apiEndpoint = "https://api.iterable.com/api"
         static let deleteUser = "/users/"
+        static let inAppTarget = "/inApp/target"
+    }
+    
+    private enum Key {
+        static let inAppRecipientEmail = "recipientEmail"
+        static let inAppCampaignId = "campaignId"
     }
     
     private static let apiKey = Environment.get(key: .apiKey)!
@@ -44,3 +65,37 @@ struct IterableAPISupport {
         URLSession(configuration: URLSessionConfiguration.default)
     }()
 }
+
+class E2EDependencyContainer: DependencyContainerProtocol {
+    let dateProvider: DateProviderProtocol = SystemDateProvider()
+    let networkSession: NetworkSessionProtocol = URLSession(configuration: .default)
+    let notificationStateProvider: NotificationStateProviderProtocol = MockNotificationStateProvider(enabled: true)
+    let localStorage: LocalStorageProtocol = UserDefaultsLocalStorage()
+    let inAppDisplayer: InAppDisplayerProtocol = InAppDisplayer()
+    let inAppPersister: InAppPersistenceProtocol = InAppFilePersister()
+    let urlOpener: UrlOpenerProtocol = AppUrlOpener()
+    let applicationStateProvider: ApplicationStateProviderProtocol = UIApplication.shared
+    let notificationCenter: NotificationCenterProtocol = NotificationCenter.default
+    let apnsTypeChecker: APNSTypeCheckerProtocol = APNSTypeChecker()
+    
+    func createInAppFetcher(apiClient: ApiClientProtocol) -> InAppFetcherProtocol {
+        return InAppFetcher(apiClient: apiClient)
+    }
+}
+
+extension IterableAPIInternal {
+    @discardableResult static func initializeForE2E(apiKey: String,
+                                                    launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil,
+                                                    config: IterableConfig = IterableConfig()) -> IterableAPIInternal {
+        let e2eDependencyContainer = E2EDependencyContainer()
+        let internalImplementation = IterableAPIInternal(apiKey: apiKey,
+                                                         launchOptions: launchOptions,
+                                                         config: config,
+                                                         dependencyContainer: e2eDependencyContainer)
+        
+        internalImplementation.start().wait()
+        
+        return internalImplementation
+    }
+}
+
