7a51d6cdb7c4032698c23fc2a5d1336af77a3023
Tapash Majumder <tapash@iterable.com>
2020-07-22 21:19:42 +0530

    Make IterableTask immutable.

--- swift-sdk/Internal/IterableTask.swift
+++ swift-sdk/Internal/IterableTask.swift
@@ -7,14 +7,34 @@ import Foundation
 
 struct IterableTask {
     let id: String
-    var created: Date?
-    var modified: Date?
+    let created: Date?
+    let modified: Date?
     let processor: String
-    var attempts: Int = 0
-    var lastAttempt: Date?
-    var processing: Bool = false
-    var scheduleTime: Date?
-    var data: Data?
+    let attempts: Int
+    let lastAttempt: Date?
+    let processing: Bool
+    let scheduleTime: Date?
+    let data: Data?
+    
+    init(id: String,
+         created: Date? = nil,
+         modified: Date? = nil,
+         processor: String,
+         attempts: Int = 0,
+         lastAttempt: Date? = nil,
+         processing: Bool = false,
+         scheduleTime: Date? = nil,
+         data: Data? = nil) {
+        self.id = id
+        self.created = created
+        self.modified = modified
+        self.processor = processor
+        self.attempts = attempts
+        self.lastAttempt = lastAttempt
+        self.processing = processing
+        self.scheduleTime = scheduleTime
+        self.data = data
+    }
     
     func updated(attempts: Int? = nil,
                  lastAttempt: Date? = nil,
