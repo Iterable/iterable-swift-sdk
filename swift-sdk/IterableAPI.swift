5868252004b8adb74de07faa1941a82205eab15d
Tapash Majumder <tapash@iterable.com>
2020-08-21 12:23:30 +0530

    Release patch version 6.2.9

--- swift-sdk/IterableAPI.swift
+++ swift-sdk/IterableAPI.swift
@@ -9,7 +9,7 @@ import UIKit
 
 @objcMembers public final class IterableAPI: NSObject {
     // Current SDK Version.
-    public static let sdkVersion = "6.2.8"
+    public static let sdkVersion = "6.2.9"
     
     // MARK: Initialization
     
