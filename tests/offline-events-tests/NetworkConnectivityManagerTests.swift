d70d6a9f21f6f633694ffabc0ab1ec6b26ebcbcb
Tapash Majumder <tapash@iterable.com>
2020-09-09 15:16:14 +0530

    Unit tests for foreground/background change.

--- tests/offline-events-tests/NetworkConnectivityManagerTests.swift
+++ tests/offline-events-tests/NetworkConnectivityManagerTests.swift
@@ -125,4 +125,47 @@ class NetworkConnectivityManagerTests: XCTestCase {
         }
         wait(for: [expectation3], timeout: 1.0)
     }
+
+    func testForegroundBackgroundChange() throws {
+        let networkSession = MockNetworkSession()
+        let checker = NetworkConnectivityChecker(networkSession: networkSession)
+        let monitor = PollingNetworkMonitor(pollingInterval: 0.5)
+        let notificationCenter = MockNotificationCenter()
+        let manager = NetworkConnectivityManager(networkMonitor: monitor,
+                                                 connectivityChecker: checker,
+                                                 notificationCenter: notificationCenter)
+        
+        // check online status before everything
+        XCTAssertTrue(manager.isOnline)
+        
+        // check that status is offline when there is network error
+        let expectation1 = expectation(description: "ConnectivityManager: check status change on network error")
+        networkSession.error = IterableError.general(description: "Mock error")
+        manager.connectivityChangedCallback = { connected in
+            XCTAssertFalse(connected)
+            expectation1.fulfill()
+        }
+        manager.start()
+        wait(for: [expectation1], timeout: 10.0)
+        
+        // check that status is still offline when app is in background, even though network is normal.
+        notificationCenter.post(name: UIApplication.didEnterBackgroundNotification, object: nil, userInfo: nil)
+        let expectation2 = expectation(description: "ConnectivityManager: check no status change on network back to normal")
+        expectation2.isInverted = true
+        manager.connectivityChangedCallback = { connected in
+            XCTAssertTrue(connected)
+            expectation2.fulfill()
+        }
+        networkSession.error = nil
+        wait(for: [expectation2], timeout: 1.0)
+
+        // check that status changes when we go online
+        let expectation3 = expectation(description: "ConnectivityManager: status change when app goes to foreground")
+        manager.connectivityChangedCallback = { connected in
+            XCTAssertTrue(connected)
+            expectation3.fulfill()
+        }
+        notificationCenter.post(name: UIApplication.willEnterForegroundNotification, object: nil, userInfo: nil)
+        wait(for: [expectation3], timeout: 10.0)
+    }
 }
