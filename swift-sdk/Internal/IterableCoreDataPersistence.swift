291d8baaf1d6fd59021906b0aa71d9479c2d6471
Tapash Majumder <tapash@iterable.com>
2020-10-16 23:18:56 +0530

    Remove unneeded.

--- swift-sdk/Internal/IterableCoreDataPersistence.swift
+++ swift-sdk/Internal/IterableCoreDataPersistence.swift
@@ -22,34 +22,6 @@ enum PersistenceConst {
     }
 }
 
-/// `Bundle.current` is used to find url path for core data model file.
-/// This is a temporary fix until we can use `Bundle.module` in IterableSDK.
-import class Foundation.Bundle
-private class BundleFinder {}
-extension Foundation.Bundle {
-    /// Returns the resource bundle associated with the current Swift module.
-    static var current: Bundle = {
-        // This is your `target.path` (located in your `Package.swift`) by replacing all the `/` by the `_`.
-        let bundleName = "IterableSDK_IterableSDK"
-        let candidates = [
-            // Bundle should be present here when the package is linked into an App.
-            Bundle.main.resourceURL,
-            // Bundle should be present here when the package is linked into a framework.
-            Bundle(for: BundleFinder.self).resourceURL,
-            // For command-line tools.
-            Bundle.main.bundleURL,
-        ]
-        for candidate in candidates {
-            let bundlePath = candidate?.appendingPathComponent(bundleName + ".bundle")
-            if let bundle = bundlePath.flatMap(Bundle.init(url:)) {
-                return bundle
-            }
-        }
-        
-        return Bundle(for: BundleFinder.self)
-    }()
-}
-
 @available(iOS 10.0, *)
 class PersistentContainer: NSPersistentContainer {
     static let shared: PersistentContainer? = {
