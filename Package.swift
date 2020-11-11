650ca7b3b241df74a33c574f44bac37c4f6f0209
Jay Kim <jay@nullavenue.com>
2020-11-10 17:48:46 -0800

    zip frameworks correctly, add references to SPM

--- Package.swift
+++ Package.swift
@@ -27,5 +27,11 @@ let package = Package(
         .target(name: "IterableAppExtensions",
                 path: "notification-extension",
                 exclude: ["Info.plist"]),
+        .binaryTarget(name: "IterableSDK",
+                      url: "https://github.com/Iterable/swift-sdk/releases/download/6.2.15/IterableSDK.xcframework.zip",
+                      checksum: "4ac30cc9b678c555ada254b9dd57d72778ebbce12b7d0b4da3b52937a199703a"),
+        .binaryTarget(name: "IterableAppExtensions",
+                      url: "https://github.com/Iterable/swift-sdk/releases/download/6.2.15/IterableAppExtensions.xcframework.zip",
+                      checksum: "401eef0dc84173aaeb1a6e93b841d99409202d0b311bab0cc7c2aee3aee3e54e"),
     ]
 )
