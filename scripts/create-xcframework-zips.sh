815167c83a0c588eeff180d1c6eb071bfc810006
Jay Kim <jay@nullavenue.com>
2020-11-16 11:39:31 -0800

    fix up script to use archival method

--- scripts/create-xcframework-zips.sh
+++ scripts/create-xcframework-zips.sh
@@ -10,27 +10,32 @@ OUTPUT_FOLDER=build
 
 rm -rf "${OUTPUT_FOLDER}"
 
-xcodebuild -target "swift-sdk" -configuration Release ONLY_ACTIVE_ARCH=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES BUILD_DIR="${OUTPUT_FOLDER}" BUILD_ROOT="${OUTPUT_FOLDER}" BITCODE_GENERATION_MODE=bitcode build -sdk iphoneos
-xcodebuild -target "swift-sdk" -configuration Release ONLY_ACTIVE_ARCH=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES BUILD_DIR="${OUTPUT_FOLDER}" BUILD_ROOT="${OUTPUT_FOLDER}" BITCODE_GENERATION_MODE=bitcode build -sdk iphonesimulator
-xcodebuild -target "notification-extension" -configuration Release ONLY_ACTIVE_ARCH=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES BUILD_DIR="${OUTPUT_FOLDER}" BUILD_ROOT="${OUTPUT_FOLDER}" BITCODE_GENERATION_MODE=bitcode build -sdk iphoneos
-xcodebuild -target "notification-extension" -configuration Release ONLY_ACTIVE_ARCH=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES BUILD_DIR="${OUTPUT_FOLDER}" BUILD_ROOT="${OUTPUT_FOLDER}" BITCODE_GENERATION_MODE=bitcode build -sdk iphonesimulator
+# -destination="iOS" -- seems like "-sdk iphoneos" is needed and not this?
+xcodebuild archive -scheme "swift-sdk" -archivePath SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES "./${OUTPUT_FOLDER}/IterableSDK-iOS" -sdk iphoneos
 
-xcodebuild -create-xcframework \
--output "${OUTPUT_FOLDER}/IterableSDK.xcframework" \
--framework "${OUTPUT_FOLDER}/Release-iphoneos/IterableSDK.framework" \
--framework "${OUTPUT_FOLDER}/Release-iphonesimulator/IterableSDK.framework"
+# -destination="iOS Simulator" -- seems like "-sdk iphonesimulator" is needed and not this?
+xcodebuild archive -scheme "swift-sdk" -archivePath SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES "./${OUTPUT_FOLDER}/IterableSDK-Simulator" -sdk iphonesimulator
 
+# create IterableSDK.xcframework
 xcodebuild -create-xcframework \
--output "${OUTPUT_FOLDER}/IterableAppExtensions.xcframework" \
--framework "${OUTPUT_FOLDER}/Release-iphoneos/IterableAppExtensions.framework" \
--framework  "${OUTPUT_FOLDER}/Release-iphonesimulator/IterableAppExtensions.framework"
+-output "./${OUTPUT_FOLDER}/IterableSDK.xcframework" \
+-framework "./${OUTPUT_FOLDER}/IterableSDK-iOS.xcarchive/Products/Library/Frameworks/IterableSDK.framework" \
+-framework "./${OUTPUT_FOLDER}/IterableSDK-Simulator.xcarchive/Products/Library/Frameworks/IterableSDK.framework"
+
+# create IterableAppExtensions.xcframework
+xcodebuild -create-xcframework \
+-output "./${OUTPUT_FOLDER}/IterableAppExtensions.xcframework" \
+-framework "./${OUTPUT_FOLDER}/IterableSDK-iOS.xcarchive/Products/Library/Frameworks/IterableAppExtensions.framework" \
+-framework "./${OUTPUT_FOLDER}/IterableSDK-Simulator.xcarchive/Products/Library/Frameworks/IterableAppExtensions.framework"
 
 cd "${OUTPUT_FOLDER}"
 
 zip -r "IterableSDK.xcframework.zip" "IterableSDK.xcframework"
 zip -r "IterableAppExtensions.xcframework.zip" "IterableAppExtensions.xcframework"
 
+echo "----------------------------------------------------------------"
+
 swift package compute-checksum "IterableSDK.xcframework.zip"
 swift package compute-checksum "IterableAppExtensions.xcframework.zip"
 
-echo "end"
+echo "----------------------------------------------------------------"
