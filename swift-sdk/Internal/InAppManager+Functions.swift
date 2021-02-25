fee29003eb70bf2f55624645d48904280d6f493d
Jay Kim <jay@nullavenue.com>
2021-02-24 16:42:54 -0800

    remove any read messages from being popped up

--- swift-sdk/Internal/InAppManager+Functions.swift
+++ swift-sdk/Internal/InAppManager+Functions.swift
@@ -71,6 +71,7 @@ struct MessagesProcessor {
     private func getFirstProcessableTriggeredMessage() -> IterableInAppMessage? {
         messagesMap.values
             .filter(MessagesProcessor.isProcessableTriggeredMessage)
+            .filter({ !$0.read })
             .sorted { $0.priorityLevel < $1.priorityLevel }
             .first
     }
