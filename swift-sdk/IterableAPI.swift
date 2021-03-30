8ea5f9995462500ccdcf60399997fec7b83064b4
Jay Kim <jay@nullavenue.com>
2021-03-30 15:53:12 -0700

    version 6.2.21

--- swift-sdk/IterableAPI.swift
+++ swift-sdk/IterableAPI.swift
@@ -10,7 +10,7 @@ import UIKit
 @objcMembers
 public final class IterableAPI: NSObject {
     /// The current SDK version
-    public static let sdkVersion = "6.2.20"
+    public static let sdkVersion = "6.2.21"
     
     /// The email of the logged in user that this IterableAPI is using
     public static var email: String? {
