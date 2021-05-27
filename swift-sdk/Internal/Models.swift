9a4e81de5fcc06818da9ae76f2d59001df737924
Jay Kim <jay@nullavenue.com>
2021-05-27 16:59:58 -0700

    revert to using beta flag

--- swift-sdk/Internal/Models.swift
+++ swift-sdk/Internal/Models.swift
@@ -6,7 +6,7 @@
 import Foundation
 
 struct RemoteConfiguration: Codable, Equatable {
-    static let isBeta = false
+    static let isBeta = true
     
     let offlineMode: Bool
     let offlineModeBeta: Bool
