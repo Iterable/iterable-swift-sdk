1353af8d202c0372bd7f763c44975f313ceec18d
Jay Kim <jay@nullavenue.com>
2021-07-14 15:57:44 -0700

    split out in-app message and manager classes

---
+++ swift-sdk/IterableInAppManagerProtocol.swift
@@ -0,0 +1,48 @@
+//
+//  Copyright © 2021 Iterable. All rights reserved.
+//
+
+@objc public protocol IterableInAppManagerProtocol {
+    /// Turn on/off automatic displaying of in-apps
+    /// - remark: the default value is `false`
+    /// - remark: if auto displaying is turned on, the SDK will also immediately retrieve and process in-apps
+    var isAutoDisplayPaused: Bool { get set }
+    
+    /// - returns: A list of all in-app messages
+    @objc func getMessages() -> [IterableInAppMessage]
+    
+    /// - returns: A list of all inbox messages
+    @objc func getInboxMessages() -> [IterableInAppMessage]
+    
+    /// - returns: A count of unread inbox messages
+    @objc func getUnreadInboxMessagesCount() -> Int
+    
+    /// - parameter message: The message to show.
+    @objc(showMessage:) func show(message: IterableInAppMessage)
+    
+    /// - parameter message: The message to show.
+    /// - parameter consume: Set to true to consume the event from the server queue if the message is shown. This should be default.
+    /// - parameter callback: block of code to execute once the user clicks on a link or button in the in-app notification.
+    ///   Note that this callback is called in addition to calling `IterableCustomActionDelegate` or `IterableUrlDelegate` on the button action.
+    @objc(showMessage:consume:callbackBlock:) func show(message: IterableInAppMessage, consume: Bool, callback: ITBURLCallback?)
+    
+    /// - parameter message: The message to remove.
+    @objc(removeMessage:) func remove(message: IterableInAppMessage)
+    
+    /// - parameter message: The message to remove.
+    /// - parameter source: The source of deletion `inboxSwipe` or `deleteButton`.`
+    @objc(removeMessage:location:) func remove(message: IterableInAppMessage, location: InAppLocation)
+    
+    /// - parameter message: The message to remove.
+    /// - parameter location: The location from where this message was shown. `inbox` or `inApp`.
+    /// - parameter source: The source of deletion `inboxSwipe` or `deleteButton`.`
+    @objc(removeMessage:location:source:) func remove(message: IterableInAppMessage, location: InAppLocation, source: InAppDeleteSource)
+    
+    /// - parameter read: Whether this inbox message was read
+    /// - parameter message: The inbox message
+    @objc(setRead:forMessage:) func set(read: Bool, forMessage message: IterableInAppMessage)
+    
+    /// - parameter id: The id of the message
+    /// - returns: IterableInAppMessage with the id, if it exists.
+    @objc(getMessageWithId:) func getMessage(withId id: String) -> IterableInAppMessage?
+}
