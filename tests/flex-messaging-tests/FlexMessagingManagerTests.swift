6834b8b2c4f974c5dcea2fb49f8bb6d31d825e3c
Jay Kim <jay@nullavenue.com>
2023-02-16 14:32:07 -0800

    fix and add flex messaging unit test stubs

--- tests/flex-messaging-tests/FlexMessagingManagerTests.swift
+++ tests/flex-messaging-tests/FlexMessagingManagerTests.swift
@@ -4,7 +4,7 @@
 
 import XCTest
 
-final class FlexMessagingManager: XCTestCase {
+final class FlexMessagingManagerTests: XCTestCase {
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -14,6 +14,6 @@ final class FlexMessagingManager: XCTestCase {
     }
 
     func testFlexMessagingManagerInit() throws {
-        let flexMessagingManager = FlexMessagingManager()
+        
     }
 }
