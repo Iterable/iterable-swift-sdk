3b87913d46cc9136cdbd7fbf3f09586f9097377a
Jay Kim <jay@nullavenue.com>
2023-03-09 16:36:36 -0800

    check in auto fetch timer progress

--- swift-sdk/Internal/EmbeddedMessagingManager.swift
+++ swift-sdk/Internal/EmbeddedMessagingManager.swift
@@ -22,6 +22,7 @@ class EmbeddedMessagingManager: NSObject, IterableEmbeddedMessagingManagerProtoc
     deinit {
         ITBInfo()
         
+        deinitAutoFetchTimer()
         deinitObservers()
     }
     
@@ -52,17 +53,62 @@ class EmbeddedMessagingManager: NSObject, IterableEmbeddedMessagingManagerProtoc
     }
     
     private func initObservers() {
+        // TODO: add app foreground/background switching notification registration here
+        
         
     }
     
     private func deinitObservers() {
+        // TODO: add app foreground/background switching notification removal here
+        
         
     }
     
     private func initAutoFetchTimer() {
-        
+        startAutoFetchTimer()
     }
     
+    private func deinitAutoFetchTimer() {
+        stopAutoFetchTimer()
+    }
+    
+    private func resetAutoFetchTimer() {
+        stopAutoFetchTimer()
+        
+        startAutoFetchTimer()
+    }
+    
+    private func stopAutoFetchTimer() {
+        autoFetchTimer?.invalidate()
+        autoFetchTimer = nil
+    }
+    
+    private func startAutoFetchTimer() {
+        autoFetchTimer = Timer.scheduledTimer(withTimeInterval: autoFetchInterval,
+                                              repeats: true,
+                                              block: { [weak self] _ in
+            self?.retrieveAndSyncEmbeddedMessages()
+        })
+    }
+    
+    private func retrieveAndSyncEmbeddedMessages() {
+        // Pending<[IterableEmbeddedMessage], SendRequestError>
+        apiClient.getEmbeddedMessages()
+            .map { fetchedMessages in
+                // TODO: decide if parsing errors should be accounted for here
+                
+                // TODO: diff merge comparison with local messages variable
+                // TODO: persist, if desired, or add placeholder for future
+                // TODO: notify message update delegates
+            }
+    }
+    
+    private var apiClient: ApiClientProtocol
+    
+    private var autoFetchInterval: TimeInterval
+    private var autoFetchTimer: Timer?
+    
     private var messages: [IterableEmbeddedMessage] = []
+    
     private var listeners: [String] = [] //change to protocol
 }
