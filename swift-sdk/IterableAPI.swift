02f5d48434364f1830615b030d6eedc85630b046
“Akshay <ayyanchira.akshay@gmail.com>
2023-07-07 11:12:00 -0700

    [MOB - 6480] - Prepare for release 6.4.14

--- swift-sdk/IterableAPI.swift
+++ swift-sdk/IterableAPI.swift
@@ -7,7 +7,7 @@ import UIKit
 
 @objcMembers public final class IterableAPI: NSObject {
     /// The current SDK version
-    public static let sdkVersion = "6.4.13"
+    public static let sdkVersion = "6.4.14"
     
     /// The email of the logged in user that this IterableAPI is using
     public static var email: String? {
