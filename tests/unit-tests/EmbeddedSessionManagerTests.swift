de60053f01e80eeb8ccfbe69f3c91c6299044015
anlinguist <an.linguist@gmail.com>
2023-08-24 11:46:30 -0600

    start embedded session manager tests and move embedded manager tests

---
+++ tests/unit-tests/EmbeddedSessionManagerTests.swift
@@ -0,0 +1,49 @@
+//
+//  Copyright © 2023 Iterable. All rights reserved.
+//
+
+import XCTest
+
+@testable import IterableSDK
+
+final class EmbeddedSessionManagerTests: XCTestCase {
+    // init/deinit
+        /// testManagerInitializesCorrectly
+    
+        /// testManagerDeinitializesCorrectly
+    
+    
+    // session management
+        /// testStartSession
+    
+    
+        /// testEndSession
+    
+    
+        /// testEndSession_NoActiveSession
+    
+    
+    // impression management
+        /// testStartImpression
+    
+    
+        /// testStartImpression_MultipleTimes
+    
+        
+        /// testPauseImpression
+    
+    
+        /// testPauseImpression_NoExistingImpression
+    
+    
+        /// testPauseImpression_AlreadyPaused
+    
+    
+    // time tracking
+        /// testElapsedTimeCalculation
+    
+    
+        /// testDisplayDurationUpdate
+    
+    
+}
