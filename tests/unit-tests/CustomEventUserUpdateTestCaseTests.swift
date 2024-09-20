1cf3c07a9863a8f3566fd02b79311513be4b28f2
Megha Pithadiya <megha.pithadiya@iterable.com>
2024-09-20 11:49:50 +0530

    - Add support to check for nested criteria a.b.c

--- tests/unit-tests/CustomEventUserUpdateTestCaseTests.swift
+++ tests/unit-tests/CustomEventUserUpdateTestCaseTests.swift
@@ -108,5 +108,161 @@ final class CustomEventUserUpdateTestCaseTests: XCTestCase {
         let matchedCriteriaId = CriteriaCompletionChecker(anonymousCriteria: data(from: mockData)!, anonymousEvents: eventItems).getMatchedCriteria()
         XCTAssertEqual(matchedCriteriaId, expectedCriteriaId)
     }
+
+
+    private let mockDataForMultiLevelNested = """
+    {
+        "count": 3,
+        "criteriaSets": [
+            {
+                "criteriaId": "425",
+                "name": "Multi level Nested field criteria",
+                "createdAt": 1726811375306,
+                "updatedAt": 1726811375306,
+                "searchQuery": {
+                    "combinator": "And",
+                    "searchQueries": [
+                        {
+                            "combinator": "And",
+                            "searchQueries": [
+                                {
+                                    "dataType": "customEvent",
+                                    "searchCombo": {
+                                        "combinator": "And",
+                                        "searchQueries": [
+                                            {
+                                                "dataType": "customEvent",
+                                                "field": "button-clicked.updateCart.updatedShoppingCartItems.quantity",
+                                                "comparatorType": "Equals",
+                                                "value": "10",
+                                                "fieldType": "long"
+                                            },
+                                            {
+                                                "dataType": "customEvent",
+                                                "field": "button-clicked.browserVisit.website.domain",
+                                                "comparatorType": "Equals",
+                                                "value": "https://mybrand.com/socks",
+                                                "fieldType": "string"
+                                            }
+                                        ]
+                                    }
+                                }
+                            ]
+                        }
+                    ]
+                }
+            }
+        ]
+    }
+    """
+
+    func testMultiLevelNestedFailed1() {
+        let eventItems: [[AnyHashable: Any]] = [
+            [
+              "dataType": "customEvent",
+              "createdAt": 1699246745093,
+              "eventName": "button-clicked",
+              "dataFields": [
+                "updateCart": [
+                  "updatedShoppingCartItems": [
+                    "quantity": 10
+                  ]
+                ],
+                "browserVisit": [
+                  "website.domain": "https://mybrand.com/socks"
+                ]
+              ]
+            ]
+          ]
+        let matchedCriteriaId = CriteriaCompletionChecker(anonymousCriteria: data(from: mockDataForMultiLevelNested)!, anonymousEvents: eventItems).getMatchedCriteria()
+        XCTAssertEqual(matchedCriteriaId, nil)
+    }
+
+    func testMultiLevelNestedFailed2() {
+        let eventItems: [[AnyHashable: Any]] = [
+            [
+              "dataType": "customEvent",
+              "createdAt": 1699246745093,
+              "eventName": "button-clicked",
+              "dataFields": [
+                "updateCart": [
+                  "updatedShoppingCartItems.quantity": 10
+                ],
+                "browserVisit": [
+                  "website.domain": "https://mybrand.com/socks"
+                ]
+              ]
+            ]
+          ]
+        let matchedCriteriaId = CriteriaCompletionChecker(anonymousCriteria: data(from: mockDataForMultiLevelNested)!, anonymousEvents: eventItems).getMatchedCriteria()
+        XCTAssertEqual(matchedCriteriaId, nil)
+    }
+
+    func testMultiLevelNestedFailed3() {
+        let eventItems: [[AnyHashable: Any]] = [
+            [
+              "dataType": "customEvent",
+              "createdAt": 1699246745093,
+              "eventName": "button-clicked",
+              "dataFields": [
+                "button-clicked": [
+                  "updateCart": [
+                    "updatedShoppingCartItems": [
+                      "quantity": 10
+                    ]
+                  ],
+                  "browserVisit": [
+                    "website": [
+                      "domain": "https://mybrand.com/socks"
+                    ]
+                  ]
+                ]
+              ]
+            ]
+          ]
+        let matchedCriteriaId = CriteriaCompletionChecker(anonymousCriteria: data(from: mockDataForMultiLevelNested)!, anonymousEvents: eventItems).getMatchedCriteria()
+        XCTAssertEqual(matchedCriteriaId, nil)
+    }
+
+    func testMultiLevelNestedFailed4() {
+        let eventItems: [[AnyHashable: Any]] = [
+            [
+              "dataType": "customEvent",
+              "createdAt": 1699246745093,
+              "eventName": "button-clicked",
+              "dataFields": [
+                "quantity": 10,
+                "domain": "https://mybrand.com/socks"
+              ]
+            ]
+          ]
+        let matchedCriteriaId = CriteriaCompletionChecker(anonymousCriteria: data(from: mockDataForMultiLevelNested)!, anonymousEvents: eventItems).getMatchedCriteria()
+        XCTAssertEqual(matchedCriteriaId, nil)
+    }
+
+    func testMultiLevelNestedSuccessCase() {
+        let eventItems: [[AnyHashable: Any]] = [
+            [
+              "dataType": "customEvent",
+              "createdAt": 1699246745093,
+              "eventName": "button-clicked",
+              "dataFields": [
+                "updateCart": [
+                  "updatedShoppingCartItems": [
+                    "quantity": 10
+                  ]
+                ],
+                "browserVisit": [
+                  "website": [
+                    "domain": "https://mybrand.com/socks"
+                  ]
+                ]
+              ]
+            ]
+          ]
+        let expectedCriteriaId = "425"
+        let matchedCriteriaId = CriteriaCompletionChecker(anonymousCriteria: data(from: mockDataForMultiLevelNested)!, anonymousEvents: eventItems).getMatchedCriteria()
+        XCTAssertEqual(matchedCriteriaId, expectedCriteriaId)
+    }
 }
 
