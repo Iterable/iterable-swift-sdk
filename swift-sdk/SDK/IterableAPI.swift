0743abdaeed48f4897f3f538511a6431b18cc0fe
Evan Takeo Kanaiaupuni Greer <56953678+evantk91@users.noreply.github.com>
2025-02-18 16:18:33 -0700

    [MOB-10929] updates changelog and version numbers (#901)
    Co-authored-by: Evan Greer <evan.greer@evan.greer>

--- swift-sdk/SDK/IterableAPI.swift
+++ swift-sdk/SDK/IterableAPI.swift
@@ -7,7 +7,7 @@ import UIKit
 
 @objcMembers public final class IterableAPI: NSObject {
     /// The current SDK version
-    public static let sdkVersion = "6.6.0-beta2"
+    public static let sdkVersion = "6.6.0-beta3"
     
     /// The email of the logged in user that this IterableAPI is using
     public static var email: String? {
