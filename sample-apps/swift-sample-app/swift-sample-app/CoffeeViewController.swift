37d91c822f19d0339cfd3c1dd99f453e2dfd766e
David Truong <dt@iterable.com>
2025-12-04 11:14:06 -0500

    example of how to utilize js routing for in-apps

--- sample-apps/swift-sample-app/swift-sample-app/CoffeeViewController.swift
+++ sample-apps/swift-sample-app/swift-sample-app/CoffeeViewController.swift
@@ -7,16 +7,35 @@
 //
 
 import UIKit
+import WebKit
 
 import IterableSDK
 
-class CoffeeViewController: UIViewController {
+class CoffeeViewController: UIViewController, WKScriptMessageHandler {
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+        if message.name == "textHandler",
+           let text = message.body as? String {
+
+            print("Received from HTML:", text)
+
+            // Show native alert.
+            let alert = UIAlertController(
+                title: "Applied Additional 10% Bonus Discount!",
+                message: "Zip Code: "+text,
+                preferredStyle: .alert
+            )
+            alert.addAction(UIAlertAction(title: "OK", style: .default))
+            present(alert, animated: true)
+        }
+    }
+    
     @IBOutlet weak var coffeeLbl: UILabel!
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var buyBtn: UIButton!
     @IBOutlet weak var cancelBtn: UIButton!
     
     var coffee: CoffeeType?
+    var webView: WKWebView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,6 +46,44 @@ class CoffeeViewController: UIViewController {
         
         coffeeLbl.text = coffee.name
         imageView.image = coffee.image
+        
+        // Add JS message handler
+        let contentController = WKUserContentController()
+        contentController.add(self, name: "textHandler")
+
+        let config = WKWebViewConfiguration()
+        config.userContentController = contentController
+
+        webView = WKWebView(frame: view.bounds, configuration: config)
+        webView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        view.addSubview(webView)
+        
+        loadInlineHTML()
+    }
+    
+    private func loadInlineHTML() {
+        let html = """
+        <!DOCTYPE html>
+        <html>
+        <body>
+
+        <h1>Get Additional Local Discounts</h1>
+        <input id="myInput" type="text" placeholder="Type in your zip code…" />
+        <button onclick="sendToiOS()">Apply</button>
+
+        <script>
+        function sendToiOS() {
+            var text = document.getElementById("myInput").value;
+            window.webkit.messageHandlers.textHandler.postMessage(text);
+        }
+        </script>
+
+        </body>
+        </html>
+        """
+
+        // Base URL is required for JS to run correctly
+        webView.loadHTMLString(html, baseURL: nil)
     }
     
     @IBAction func handleBuyButtonTap(_: Any) {
