043f92ec069d9715c37aa0a0b1ab29be4362c2c8
joaodordio <joaodordio@users.noreply.github.com>
2026-03-31 14:56:19 +0000

    [SDK-402]: Prepare for release 6.6.8

--- swift-sdk/SDK/IterableAPI.swift
+++ swift-sdk/SDK/IterableAPI.swift
@@ -7,7 +7,7 @@ import UIKit
 
 @objcMembers public final class IterableAPI: NSObject {
     /// The current SDK version
-    public static let sdkVersion = "6.6.7"
+    public static let sdkVersion = "6.6.8"
     
     /// The email of the logged in user that this IterableAPI is using
     public static var email: String? {
